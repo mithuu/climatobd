91bd91fc54a5da83c9b7aae1f8b075cec6c53fa2
mithuu <mh.mithuu@gmail.com>
2019-02-26 05:05:19 +0600

    Hello there!

---
+++ src/components/Credit.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Credit = () => (
+    <div>
+        <p className="credits">Copyright © 2019 || <a href="//fb.com/ajb.to">Mehedi Hasan Mithu</a></p>
+    </div>
+);
+
+export default Credit;
---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Titles = () => (
+    <div>
+        <h1 className="title-container__title">CLIMATOBD</h1>
+        <h3 className="title-container__subtitle">Get a quick look at the climate.</h3>
+    </div>
+);
+
+export default Titles;
